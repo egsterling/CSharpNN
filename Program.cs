a122536df8dfffc462356caf38b40589bac8a9c7
egster <egster@umich.edu>
2023-05-24 02:22:11 -0400

    fixes

--- Program.cs
+++ Program.cs
@@ -25,8 +25,8 @@ public class ConvolutionalLayer {
     readonly (int, int) padding;
 
     // Default stride: (1, 1) Default padding: 0
-    public ConvolutionalLayer(int H_in, int W_in, int in_channels, int out_channels)
-        : this(H_in, W_in, in_channels, out_channels, (1, 1), (0, 0)) {}
+    public ConvolutionalLayer(int in_channels, int H_in, int W_in, int out_channels)
+        : this(in_channels, H_in, W_in, out_channels, (1, 1), (0, 0)) {}
 
     public ConvolutionalLayer(int in_channels, int H_in, int W_in, int out_channels, (int, int) stride_in, (int, int) padding_in) {
         this.conv = new Layer[out_channels];
@@ -35,19 +35,7 @@ public class ConvolutionalLayer {
             this.conv[i] = new Layer(H_in, W_in, in_channels);
         }
         this.stride = stride_in;
-    }
-
-    public double innerProduct(Layer convLayer, double[,,] section) {
-        double[,,] layerVals = convLayer.getLayer;
-        double total = 0;
-        for(int c = 0; c < channels; ++c) {
-            for(int w = 0; w < width; ++w) {
-                for(int h = 0; h < height; ++h) {
-                    total += layerVals[c,w,h] * section[c,w,h];
-                }
-            }
-        }
-        return total + convLayer.getBias;
+        this.padding = padding_in;
     }
 
     public double[,,] padded(double[,,] inp) {
@@ -57,7 +45,7 @@ public class ConvolutionalLayer {
         int inpHeight = inp.GetLength(1);
         int inpWidth = inp.GetLength(2);
         var (vertPad, horPad) = padding;
-        double[,,] padded = new double[inpHeight + 2*vertPad, inpWidth + 2*horPad, channels];
+        double[,,] padded = new double[channels, inpHeight + 2*vertPad, inpWidth + 2*horPad];
         int paddedHeight = padded.GetLength(1);
         int paddedWidth = padded.GetLength(2);
         for(int c = 0; c < channels; ++c) {
@@ -75,6 +63,20 @@ public class ConvolutionalLayer {
         return padded;
     }
 
+    public double innerProduct(Layer convLayer, double[,,] section) {
+        double[,,] layerVals = convLayer.getLayer;
+        double total = 0;
+        for(int c = 0; c < channels; ++c) {
+            for(int w = 0; w < width; ++w) {
+                for(int h = 0; h < height; ++h) {
+                    total += layerVals[c,w,h] * section[c,w,h];
+                }
+            }
+        }
+        return total + convLayer.getBias;
+    }
+
+
     public double[,] convolute(double[,,] input) {
         int inpChannels = input.GetLength(0);
         if(inpChannels != channels) {
@@ -84,13 +86,12 @@ public class ConvolutionalLayer {
         input = padded(input);
         int inpHeight = input.GetLength(1);
         int inpWidth = input.GetLength(2);
-        double[,,] testLayer = conv[0].getLayer;
-        // if(check exceptions, like check if stride length works with padded input size, maybe others i forget)
+        // if(check exceptions, like check if stride length works with padded input size, maybe otherget)
+        
+
     }
 
-
 }
-
 
 public class PoolingLayer {
     
@@ -103,6 +104,19 @@ public class FullyConnected {
 
 public class Run {
     static void Main() {
-
+        double[,,] inp = {{{1, 2}, {1, 2}},
+                          {{1, 2}, {1, 2}},};
+        var cn = new ConvolutionalLayer(2, 2, 2, 2, (1, 1), (2, 1));
+        var pp = cn.padded(inp);
+        for(int i = 0; i < pp.GetLength(0); ++i) {
+            Console.WriteLine("Layer " + i);
+            for(int j = 0; j < pp.GetLength(1); ++j) {
+                for(int k = 0; k < pp.GetLength(2); ++k) {
+                    Console.Write(pp[i,j,k] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
     }
 }
